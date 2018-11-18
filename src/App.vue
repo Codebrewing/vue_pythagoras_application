c705822137fc5ee46426804924837e939f13e56c
Seongh78 <seongh7800@gmail.com>
2018-11-18 18:31:05 +0900

    add function

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <h1>현재 컴포넌트 : </h1>
     {{flag}}
-    <button @click.prevent="a()">flag</button>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click.prevent="a('Home')">Home</button>
+    <button @click.prevent="a('About')">About</button>
+    <button @click.prevent="a('Gallery')">Gallery</button>
+
+    <HelloWorld :msg="childData" v-if="!flag" />
   </div>
 </template>
 
@@ -17,12 +21,38 @@ export default {
   },
   data(){
     return{
-      flag:false
+      flag:false,
+      title: 'Home',
+      childData:null,
+      dataSet: {
+        Home: [
+          {name: 'home 1'}, {name: 'home 2'}, {name: 'home 3'}
+        ],
+        About: [
+          {name: 'About 1'}, {name: 'About 2'}, {name: 'About 3'}
+        ],
+        Gallery: [
+          {name: 'Gallery 1'}, {name: 'Gallery 2'}, {name: 'Gallery 3'}
+        ],
+
+      }
     }
   },
+
+
+  created(){
+    setTimeout(()=>{
+        this.childData = this.dataSet['Home']
+      }, 800)
+  },
+
+
   methods:{
-    a(){
-      alert(this.flag)
+    a(v){
+      this.childData = null
+      setTimeout(()=>{
+        this.childData = this.dataSet[v]
+      }, 800)
     }
   }
 }
